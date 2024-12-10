e470861fd1694e09bff3adf050caf9e535891be2
KAUSHIK1205 <chezhiworld@gmail.com>
2024-12-10 19:20:25 +0530

    Add files via upload

---
+++ countries emissions.py
@@ -0,0 +1,13 @@
+#Here are some countries' carbon footprint emissions:
+#China: The world's top carbon polluter, releasing over 14 billion tons of CO2
+#United States: The second largest carbon polluter, releasing 6 billion tons of CO2
+#India: The third largest carbon polluter, releasing 3.5 billion tons of CO2
+#European Union: Released 3.4 billion tons of CO2
+#Russia: Released 2 billion tons of CO2
+#Japan: Released 1.170 billion tons of CO2
+#Brazil: Released 1.140 billion tons of CO2
+#Iran: Released 1.130 billion tons of CO2 
+#Countries With The Highest Carbon Footprint 2024 Update ...
+#The top three countries, China, India, and the United States, accounted for 52% of the world's CO2 emissions in 2021. 
+#The United States has been the largest carbon emitter since the Industrial Revolution, releasing 422 billion metric tons of CO2 into the atmosphere. 
+#India's GHG emissions have roughly doubled since the turn of the century, reaching a record high of 2.7 GtCO₂ in 2022. The International Energy Agency (IEA) forecasts that India's share of global emissions could rise to 10% by 2030.
