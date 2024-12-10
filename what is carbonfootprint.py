dbff1fca06dde3d78c4ccda7e9e5f3c9ff65849a
KAUSHIK1205 <chezhiworld@gmail.com>
2024-12-10 19:10:51 +0530

    Add files via upload

---
+++ what is carbonfootprint.py
@@ -0,0 +1,15 @@
+#What Is a Carbon Footprint?A carbon footprint is the total amount of greenhouse gases (GHG) released into the atmosphere by an individual, organization, event, or product. It's calculated by adding up the emissions from each stage of a product or service's life, including production, use, and disposal. 
+#Carbon footprints include both direct emissions, like burning fossil fuels for driving or heating, and indirect emissions, like electricity from fossil fuels. The main contributors to carbon footprints are food, transportation, consumption, and household energy. 
+#Here are some ways to reduce your carbon footprint:
+#Use less energy and switch to renewable energy 
+#Change your travel habits 
+#Eat a plant-based diet 
+#Buy less and from more sustainable sources 
+#Turn down the heating by 1° 
+#Take short showers 
+#Turn off the water while brushing your teeth or washing dishes 
+#Unplug electronics when not in use 
+#Limit cloud storage 
+#Buy energy efficient products 
+#Recycle waste 
+#Carbon offsetting is a way to compensate for unavoidable emissions by investing in projects that reduce or capture carbon from the atmosphere. For example, an organization can invest in energy efficiency or reforestation
