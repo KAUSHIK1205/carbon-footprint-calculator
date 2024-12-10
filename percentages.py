752ed9ee9f547c56b6c8d499fbdf675fc5cec9fc
KAUSHIK1205 <chezhiworld@gmail.com>
2024-12-10 19:24:36 +0530

    Add files via upload

---
+++ percentages.py
@@ -0,0 +1,23 @@
+#Metrics of Absolute Decoupling
+
+#Change in CO2 (2000-2014) %	Change in CO2 (2000-2014) Mt	Change in Real GDP (2000-2014)	Change in Industry Share of GDP (2000-2013)
+#Austria	-3%	-2	21%	-3%
+##Bulgaria	-5%	-2	62%	2%
+#Czech Republic	-14%	-18	40%	-0.3%
+#Denmark	-30%	-17	8%	-5%#
+# Finland	-18%	-11	18%	-9%
+#France	-19%	-83	16%	-4%
+#Germany	-12%	-106	16%	-1%
+#Hungary	-24%	-14	29%	-2%
+#Ireland	-16%	-7	47%	-9%
+#Netherlands	-8%	-19	15%	-3%
+#Portugal	-23%	-16	1%	-6%
+#Romania	-22%	-21	65%	-1%
+#Slovakia	-22%	-9	75%	-3%
+#Spain	-14%	-48	20%	-8%
+#Sweden	-8%	-5	31%	-4%
+#Switzerland	-10%	-4	28%	-0.3%
+#Ukraine	-29%	-99	49%	-10%
+#United Kingdom	-20%	-120	27%	-6%
+#United States	-6%	-382	28%	-3%
+#Uzbekistan	-2%	-2	28%	10%
